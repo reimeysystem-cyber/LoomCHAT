fd638aef04d66907a01463123add6b2858540e7d
reimeysystem-cyber <reimeysystem@gmail.com>
2026-08-25 12:55:36 +0900

    Create firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+// firebase-config.example.js (GitHub公開用サンプル)
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
