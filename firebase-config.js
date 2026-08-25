8e8a2265ba626a79e22e32378d2246aeae003e85
reimeysystem-cyber <reimeysystem@gmail.com>
2026-08-25 12:54:32 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAtKhahc86s5zg8XfcgqUDiG5BSHLDGh64",
+  authDomain: "minecraft-2d284.firebaseapp.com",
+  projectId: "minecraft-2d284",
+  storageBucket: "minecraft-2d284.firebasestorage.app",
+  messagingSenderId: "92607262911",
+  appId: "1:92607262911:web:d2300a09bfabb9a24c960a"
+};
